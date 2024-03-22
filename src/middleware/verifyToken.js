66ffa4baf8d6829cfc651ed438c2fb3693fe1fa0
Momsombrathna <brathna84@gmail.com>
2024-03-22 13:15:11 +0700

    update verify token

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -1,11 +1,22 @@
 import jwt from "jsonwebtoken";
+import model from "../model/userModel.js";
 
-export const verifyToken = (req, res, next) => {
+const { userModel } = model;
+
+export const verifyToken = async (req, res, next) => {
   const token = req.header("auth-token");
-  if (!token) return res.status(401).json({ error: "Access denied" });
+  if (!token)
+    return res
+      .status(401)
+      .json({ error: "Access denied! token not found in header." });
+
   try {
     const decoded = jwt.verify(token, "your-secret-key");
-    req.userId = decoded.userId;
+    const user = await userModel.findById(decoded._id);
+    if (token !== user.token)
+      return res.status(401).json({ error: "Invalid token" });
+
+    req.userId = decoded._id;
     next();
   } catch (error) {
     res.status(401).json({ error: "Invalid token" });
